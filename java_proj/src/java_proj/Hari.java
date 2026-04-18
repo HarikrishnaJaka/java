970a5808cd59161b29bfdb2678942c0fc32ba584
HarikrishnaJaka <jakahari016@gmail.com>
2026-04-18 11:36:42 +0530

    adding one mothod

--- java_proj/src/java_proj/Hari.java
+++ java_proj/src/java_proj/Hari.java
@@ -4,13 +4,15 @@ public class Hari
 {
 	public static String greet()
 	{
-		return "hello boss how are you......!";
+		return "hello boss good morning......!";
+	}
+	public static String hello() {
+		return "hello boss how are you.....!";
 	}
 	public static void main(String[] args) 
 	{
-		
       System.out.println(greet());
-
+      System.out.println(hello());
 	}
 
 }
