889caa9ac66721c078f0c6eae2699365203fa6cd
HarikrishnaJaka <jakahari016@gmail.com>
2026-04-18 11:29:35 +0530

    initial commit

---
+++ java_proj/src/java_proj/Hari.java
@@ -0,0 +1,16 @@
+package java_proj;
+
+public class Hari
+{
+	public static String greet()
+	{
+		return "hello boss how are you......!";
+	}
+	public static void main(String[] args) 
+	{
+		
+      System.out.println(greet());
+
+	}
+
+}
